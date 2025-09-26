bf69c64f80aaa524bbfac02bda0d52e091fc8695
kimzooeun <asdo6863@naver.com>
2025-09-26 15:40:58 +0900

    aaa

---
+++ Q0922/src/main/java/com/example/demo/simulator/MemorySimulatorService.java
@@ -0,0 +1,11 @@
+package com.example.demo.simulator;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MemorySimulatorService {
+
+}
